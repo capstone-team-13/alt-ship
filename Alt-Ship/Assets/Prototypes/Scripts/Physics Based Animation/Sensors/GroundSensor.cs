b410f00bc63100e3542683828d8d91f81e887435
Rui Jiang <anver.jiangrui@gmail.com>
2024-10-04 00:14:01 -0400

    Finish hopper model partially

---
+++ Alt-Ship/Assets/Prototypes/Scripts/Physics Based Animation/Sensors/GroundSensor.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace EE.Prototype.PBA
+{
+    public class GroundSensor : MonoBehaviour
+    {
+        public bool IsGrounded()
+        {
+            return false;
+        }
+    }
+}
--- Alt-Ship/Assets/Prototypes/Scripts/Physics Based Animation/Tests/HopperSimulator.cs
+++ Alt-Ship/Assets/Prototypes/Scripts/Physics Based Animation/Tests/HopperSimulator.cs
@@ -4,75 +4,123 @@ using UnityEngine;
 
 namespace EE.Prototype.PBA
 {
+    [RequireComponent(typeof(Rigidbody))]
     public class HopperSimulator : MonoBehaviour
     {
-        [Header("Sensors")] private CollisionSensor m_groundSensor;
+        [Header("Configs")] [SerializeField] private float m_speed;
+        [SerializeField] private float m_springConstant;
+        [SerializeField] private float m_legRestLength;
+        [SerializeField] private float m_thrustForce;
 
-        private float3 m_targetVelocity;
+        [Header("Sensors")] [SerializeField] private GroundSensor m_groundSensor;
 
-        [Header("State Management")] private int m_stateIndex;
-        private const int MAX_STATE_COUNT = 2;
-        private System.Action[] m_onFixedUpdate;
+
+        private Rigidbody m_rigidbody;
+
+        /// <summary>
+        /// XZ Plane
+        /// </summary>
+        private float2 m_velocity;
+
+        /// <summary>
+        /// XZ Plane
+        /// </summary>
+        private float2 m_targetVelocity;
+
+        private float3 m_eulerAngles;
+        private float3 m_angularVelocity;
+
+        private bool m_wasGrounded;
+        private bool m_isGrounded;
+        private float m_lastGroundedTime;
+
 
         #region Unity Callbacks
 
         [UsedImplicitly]
         private void Awake()
         {
-            m_onFixedUpdate = new System.Action[] { __M_Stance, __M_Flight };
-        }
-
-        [UsedImplicitly]
-        private void OnEnable()
-        {
-            m_groundSensor.OnCollided.AddListener(__M__OnGround);
-        }
-
-        [UsedImplicitly]
-        private void OnDisable()
-        {
-            m_groundSensor.OnCollided.RemoveListener(__M__OnGround);
-        }
-
-        [UsedImplicitly]
-        private void OnDestroy()
-        {
-            m_groundSensor.OnCollided.RemoveListener(__M__OnGround);
+            m_rigidbody = GetComponent<Rigidbody>();
         }
 
         [UsedImplicitly]
         private void FixedUpdate()
         {
-            m_onFixedUpdate[m_stateIndex]();
+            __M_HandleInput();
+
+            m_isGrounded = m_groundSensor.IsGrounded();
+
+            __M_UpdateRotation();
+            __M_UpdateTransform();
+            __M_UpdateSpring();
+            __M_UpdateJoints();
+
+            __M_UpdateLastGroundedTime();
+            m_wasGrounded = m_isGrounded;
+
+            __M_UpdateVelocity();
+            __M_UpdateState();
         }
 
         #endregion
 
         #region Internal
 
-        private void __M__OnGround(Collision collisionInfo)
+        private void __M_HandleInput()
         {
-            Debug.Log("Collided");
+            float horizontalInput = Input.GetAxisRaw("Horizontal");
+            float verticalInput = Input.GetAxisRaw("Vertical");
+            var input = new Vector2(horizontalInput, verticalInput);
+
+            // Update velocity
+            m_targetVelocity = input * (m_speed * Time.fixedDeltaTime);
         }
 
-        private void __M_SwitchState()
+        private void __M_UpdateRotation()
         {
-            ++m_stateIndex;
-            m_stateIndex %= MAX_STATE_COUNT;
+            float3 currentEulerAngles = transform.eulerAngles;
+            float3 currentAngularVelocity = (currentEulerAngles - m_eulerAngles) / Time.fixedDeltaTime;
+
+            m_eulerAngles = currentEulerAngles;
+            m_angularVelocity = (m_angularVelocity + currentEulerAngles) * 0.5f;
         }
 
-        private void __M_SwitchState(int index)
-        {
-            m_stateIndex = index;
-        }
-
-        private void __M_Stance()
+        // TODO: Transformation
+        private void __M_UpdateTransform()
         {
         }
 
-        private void __M_Flight()
+        private void __M_UpdateSpring()
+        {
+            float currentLength;
+            float3 springVelocity;
+        }
+
+        private void __M_UpdateJoints()
         {
         }
+
+        private void __M_UpdateVelocity()
+        {
+        }
+
+        private void __M_UpdateLastGroundedTime()
+        {
+        }
+
+        #region State Management
+
+        private int m_stateIndex;
+
+        // Flight -> Landing -> Compression -> Thrust -> Unloading
+        private const int MAX_STATE_COUNT = 5;
+        private System.Action[] m_onFixedUpdate;
+
+        private void __M_UpdateState()
+        {
+        }
+
+        #endregion
 
         #endregion
     }
