824859dc237799d55886ed29729328f81106edee
Rui Jiang <anver.jiangrui@gmail.com>
2024-10-28 13:14:25 -0400

    Implement scene transition function & set up Level #1 scene

---
+++ Alt-Ship/Assets/Scripts/UI/SceneLoader.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private enum SceneName
+    {
+        Start = 0,
+        LevelSelection,
+        Level1,
+        Undefined
+    }
+
+    private static readonly string[] m_sceneNames =
+    {
+        "Start",
+        "Level Selection",
+        "Level #1"
+    };
+
+    [SerializeField] private SceneName m_sceneName = SceneName.Undefined;
+
+    public void Load()
+    {
+#if UNITY_EDITOR
+        if (m_sceneName == SceneName.Undefined)
+        {
+            Debug.LogWarning($"You need to define which scene you want to load. ({gameObject.name})");
+        }
+#endif
+        var sceneIndex = (int)m_sceneName;
+        SceneManager.LoadScene(m_sceneNames[sceneIndex]);
+    }
+}
