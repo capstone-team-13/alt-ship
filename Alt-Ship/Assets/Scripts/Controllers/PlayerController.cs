bdd1b8470bcd9af581461bb26513f7adfedeec28
Remaek <brooksmt@sheridancollege.ca>
2024-11-06 22:35:23 -0500

    Multiplayer System Integration
    Implemented the multiplayer system into the current scene.
    
    Implemented the camera and movement system into the new input system.

--- Alt-Ship/Assets/Scripts/Controllers/PlayerController.cs
+++ Alt-Ship/Assets/Scripts/Controllers/PlayerController.cs
@@ -1,12 +1,43 @@
 using EE.AMVCC;
 using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using Application = EE.AMVCC.Application;
 
 public class PlayerController : Controller<PlayerModel>
 {
     [Header("Concrete Reference")] [SerializeField]
     private Rigidbody m_rigidBody;
+
+    private InputActionAsset inputAsset;
+    private InputActionMap player;
+    private InputAction move;
+    private InputAction look;
+
+    public Material playerOneMat;
+    public Material playerTwoMat;
+    public MeshRenderer meshRenderer;
+
+    private Vector2 moveDir;
+
+    public Camera cinemachineCam;
+
+
+    private void Awake()
+    {
+        PlayerCounter.playerCount += 1;
+        inputAsset = this.GetComponent<PlayerInput>().actions;
+        player = inputAsset.FindActionMap("PlayerMovement");
+
+        if (PlayerCounter.playerCount == 1)
+        {
+            meshRenderer.material = playerOneMat;
+        }
+        else if (PlayerCounter.playerCount == 2)
+        {
+            meshRenderer.material = playerTwoMat;
+        }
+    }
 
     #region Unity Callbacks
 
@@ -15,14 +46,56 @@ public class PlayerController : Controller<PlayerModel>
     {
         if (Model.Dead) return;
 
+        if (player.enabled)
+        {
+            MovePlayer();
+        }
+    }
+
+    #endregion
+
+    private void OnEnable()
+    {
+        look = player.FindAction("Camera");
+        move = player.FindAction("Movement");
+        player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        player.Disable();
+    }
+
+    void MovePlayer()
+    {
+        Vector2 movementInput = PlayerCtrlInput();
+
+        Vector3 camForward = cinemachineCam.transform.forward;
+        Vector3 camRight = cinemachineCam.transform.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+        camForward.Normalize();
+        camRight.Normalize();
+
+        Vector3 moveDir = (camForward * movementInput.y + camRight * movementInput.x).normalized;
+
         // TODO: Refactor to new input system
-        var input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        var input = moveDir;
         __M_UpdateDirection(input);
 
         transform.Translate(Model.Velocity * Time.deltaTime);
     }
 
-    #endregion
+    private Vector2 PlayerCtrlInput()
+    {
+        Vector2 moveV = Vector2.zero;
+
+        moveV = new Vector2(move.ReadValue<Vector2>().x, move.ReadValue<Vector2>().y);
+
+        return moveV;
+    }
+
 
     #region API
 
--- Alt-Ship/Assets/UnityInputSystem/TempCharacterController/Scripts/CameraController.cs
+++ Alt-Ship/Assets/UnityInputSystem/TempCharacterController/Scripts/CameraController.cs
@@ -11,36 +11,35 @@ public class CameraController : MonoBehaviour
     [SerializeField]
     private List<LayerMask> playerLayers;
 
-    private PlayerInputManager playerInputManager;
+    public PlayerInputManager playerInputManager;
 
     private InputActionAsset inputAsset;
     private InputActionMap inputMap;
 
 
-    private void Awake()
-    {
-        playerInputManager = FindObjectOfType<PlayerInputManager>();
-    }
-
     private void OnEnable()
     {
+        Debug.Log("Hello world 1");
         playerInputManager.onPlayerJoined += AddPlayer;
     }
 
     private void OnDisable()
     {
+        Debug.Log("Hello world 2");
         playerInputManager.onPlayerLeft -= AddPlayer;
     }
 
     public void AddPlayer(PlayerInput player)
     {
+        Debug.Log("Hello world");
         players.Add(player);
         int layerToAdd = (int)Mathf.Log(playerLayers[players.Count - 1].value, 2);
 
         inputAsset = player.GetComponent<PlayerInput>().actions;
+        
         inputMap = inputAsset.FindActionMap("PlayerMovement");
 
-        Transform playerParent = player.transform.parent; 
+        Transform playerParent = player.transform; 
 
         playerParent.GetComponentInChildren<CinemachineFreeLook>().gameObject.layer = layerToAdd;
         playerParent.GetComponentInChildren<Camera>().cullingMask |= 1 << layerToAdd;
