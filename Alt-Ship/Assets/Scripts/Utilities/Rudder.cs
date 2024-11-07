97704fe0b72547e7074ac74f7b38c57d40e1928b
Remaek <brooksmt@sheridancollege.ca>
2024-11-07 16:00:34 -0500

    Added player steering
    - Users can steer the ship and interact with stations without interfering with the other players.

--- Alt-Ship/Assets/Scripts/Utilities/Rudder.cs
+++ Alt-Ship/Assets/Scripts/Utilities/Rudder.cs
@@ -2,10 +2,40 @@ using EE.Interactions;
 using JetBrains.Annotations;
 using UnityEngine;
 using Application = EE.AMVCC.Application;
+using UnityEngine.InputSystem;
 
 public class Rudder : MonoBehaviour
 {
+    public InputActionAsset inputActions;
+    private InputAction steering;
+
+
     private bool m_steering;
+    private float rotationSign;
+
+    private void Awake()
+    {
+        var actionMap = inputActions.FindActionMap("Steering");
+        steering = actionMap.FindAction("ShipSteering");
+    }
+
+    void OnEnable()
+    {
+        rotationSign = 0.0f;
+        steering.Enable();
+        steering.performed += steeringBoat;
+        steering.canceled += steeringCanceled;
+    }
+
+    private void OnDisable()
+    {
+        steering.performed -= steeringBoat;
+        steering.canceled -= steeringCanceled;
+        rotationSign = 0.0f;
+        steering.Disable();
+
+    }
+
 
     #region Unity Callbacks
 
@@ -15,16 +45,24 @@ public class Rudder : MonoBehaviour
         if (!m_steering) return;
 
         // TODO: Refactor using new input system
-        var rotationSign = 0.0f;
-        if (Input.GetKey(KeyCode.J)) rotationSign = -1;
-        else if (Input.GetKey(KeyCode.K)) rotationSign = 1;
 
-        if (rotationSign != 0)
+        if (rotationSign != 0f)
         {
             Debug.Log("Steering");
             Application.Instance.Push(new ShipCommand.Steer(rotationSign));
         }
     }
+
+    private void steeringBoat(InputAction.CallbackContext context)
+    {
+        rotationSign = context.ReadValue<float>();
+    }
+
+    private void steeringCanceled(InputAction.CallbackContext context)
+    {
+        rotationSign = 0.0f;
+    }
+
 
     #endregion
 
