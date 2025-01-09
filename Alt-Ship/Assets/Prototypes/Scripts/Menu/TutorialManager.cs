efde337cba34631de608669123c9af784a0fdb15
JackYou0706 <jackyou233@gmail.com>
2025-01-08 20:39:52 -0500

    Tutorial panel+prefab

---
+++ Alt-Ship/Assets/Prototypes/Scripts/Menu/TutorialManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    public GameObject tutorialPanel;
+
+    // 显示教程面板
+    public void ShowTutorial()
+    {
+        tutorialPanel.SetActive(true);
+    }
+
+    // 隐藏教程面板
+    public void HideTutorial()
+    {
+        tutorialPanel.SetActive(false);
+    }
+}
