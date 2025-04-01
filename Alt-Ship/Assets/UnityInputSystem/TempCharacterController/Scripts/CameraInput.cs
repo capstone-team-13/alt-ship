be875f8a7cd46eb27a3af3934b0923acd1b18e08
Remaek <brooksmt@sheridancollege.ca>
2025-03-31 22:54:52 -0400

    Player Two Camera Fix

--- Alt-Ship/Assets/UnityInputSystem/TempCharacterController/Scripts/CameraInput.cs
+++ Alt-Ship/Assets/UnityInputSystem/TempCharacterController/Scripts/CameraInput.cs
@@ -1,54 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 using UnityEngine.InputSystem;
 
 public class CameraInput : MonoBehaviour, AxisState.IInputAxisProvider
 {
-    [HideInInspector] public InputAction horizontal;
-    [HideInInspector] public InputAction vertical;
+    [HideInInspector]
+    public InputAction horizontal;
+    [HideInInspector]
+    public InputAction verticle;
 
-    private static CameraInput _instance;
-    public static CameraInput Instance => _instance;
-
-    private Vector2 inputValues;
-
-    private void Awake()
-    {
-        if (_instance == null)
-            _instance = this;
-        else
-        {
-            Destroy(gameObject);
-            return;
-        }
-    }
-
-    private void OnEnable()
-    {
-        horizontal.Enable();
-        vertical.Enable();
-    }
-
-    private void OnDisable()
-    {
-        horizontal.Disable();
-        vertical.Disable();
-    }
-
-    private void FixedUpdate()
-    {
-        // Read input values in FixedUpdate for consistency
-        inputValues = horizontal.ReadValue<Vector2>();
-    }
 
     public float GetAxisValue(int axis)
     {
-        return axis switch
+        switch (axis)
         {
-            0 => inputValues.x, // Horizontal Input
-            1 => inputValues.y, // Vertical Input
-            2 => vertical.ReadValue<float>(), // Scroll / Other
-            _ => 0
-        };
+            case 0: return horizontal.ReadValue<Vector2>().x;
+            case 1: return horizontal.ReadValue<Vector2>().y;
+            case 2: return verticle.ReadValue<float>();
+        }
+        return 0;
     }
+
 }
